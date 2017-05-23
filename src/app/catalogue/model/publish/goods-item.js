5561b0a6af7e48f1324332b4832fb7154d89bddf
Oliver Jung <oliver.jung@salzburgresearch.at>
2017-05-23 12:32:34 +0200

    Removed compiled files from GIT for clarity and added subfolder files to gitignore

--- src/app/catalogue/model/publish/goods-item.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class GoodsItem {
-    constructor(id, item) {
-        this.id = id;
-        this.item = item;
-    }
-}
-exports.GoodsItem = GoodsItem;
-//# sourceMappingURL=goods-item.js.map
