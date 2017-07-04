dffb48b74a4f39bf03ceeb8ee2f9328df10bee34
Shantanoo Desai <des@biba.uni-bremen.de>
2017-07-04 14:25:05 +0200

    Add Stable Diagram Rendering with User Input

--- src/app/explorative-search/explorative-search-details.component.ts
+++ src/app/explorative-search/explorative-search-details.component.ts
@@ -6,13 +6,13 @@
  * The page needs to be refreshed for the next query
  */
 
-import { Component, AfterViewInit, ViewChild , ElementRef, Input, OnChanges } from '@angular/core';
+import { Component, AfterViewInit, ViewChild , ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
 import * as go from 'gojs';
 import { RadialLayout } from './layout/RadialLayout';
 
 @Component({
     selector: 'explore-search-details',
-    template: `<p>{{name}}</p><div class="displayArea" #myDiagramDiv></div>`,
+    template: `<div class="displayArea" #myDiagramDiv></div>`,
     styleUrls: ['./explorative-search-form.component.css']
 })
 
@@ -20,96 +20,57 @@ import { RadialLayout } from './layout/RadialLayout';
 export class ExplorativeSearchDetailsComponent implements AfterViewInit, OnChanges {
 
     @Input() config: Object[];
-    @Input() name: string;
     // GoJS div as mentioned above in the @Component `template` param
     @ViewChild('myDiagramDiv') div: ElementRef;
 
     private myDiagram: go.Diagram;
-    private $: any = go.GraphObject.make;
-    private names: string[] = [];
+    private $ = go.GraphObject.make;
+
     constructor() {}
-    ngOnChanges(): void {
+
+    ngOnChanges(changes: SimpleChanges): void {
         if (!this.config || this.config.length === 0) { return; }
-        console.log(this.config);
-        this.myDiagram = null;
-        this.figSetup();
+        // console.log(JSON.stringify(changes));
+        // console.log(JSON.stringify(this.config));
+        this.generateGraph();
     }
 
     ngAfterViewInit(): void {
-    }
-
-    private figSetup(): void {
-            this.myDiagram =  this.$(go.Diagram, this.div.nativeElement, // must be the ID or reference to div
+        this.myDiagram = this.$(go.Diagram, this.div.nativeElement,
+    {
+      initialContentAlignment: go.Spot.Center,
+      padding: 10,
+      isReadOnly: true,
+      'animationManager.isEnabled': false
+    });
+    let commonToolTip = this.$(go.Adornment, 'Auto',
             {
-                initialAutoScale: go.Diagram.Uniform,
-                initialContentAlignment: go.Spot.Center,
-                padding: 10,
-                isReadOnly: true,
-                layout: this.$(RadialLayout, {
-                maxLayers: 2,
-                rotateNode: function(node: any, angle: any, sweep: any, radius: any) {
-                    // rotate the nodes and make sure the text is not upside-down
-                    node.angle = angle;
-                    let label = node.findObject('TEXTBLOCK');
-                    if (label !== null) {
-                    label.angle = ((angle > 90 && angle < 270 || angle < -90) ? 180 : 0);
-                    }
-                },
-                commitLayers: function() {
-                    // optional: add circles in the background
-                    // need to remove any old ones first
-                    let diagram = this.diagram;
-                    let gridlayer = diagram.findLayer('Grid');
-                    let circles = new go.Set(go.Part);
-                    gridlayer.parts.each((circle: any) => {
-                    if (circle.name === 'CIRCLE') { circles.add(circle); }
-                    });
-                    circles.each(function(circle) {
-                    diagram.remove(circle);
-                    });
-                    // add circles centered at the root
-                    let $ = go.GraphObject.make;  // for conciseness in defining templates
-                    for (let lay = 1; lay <= this.maxLayers; lay++) {
-                    let radius = lay * this.layerThickness;
-                    let circle =
-                        $(go.Part,
-                        { name: 'CIRCLE', layerName: 'Grid' },
-                        { locationSpot: go.Spot.Center, location: this.root.location },
-                        $(go.Shape, 'Circle',
-                            { width: radius * 2, height: radius * 2 },
-                            { fill: 'rgba(200,200,200,0.2)', stroke: null }));
-                    diagram.add(circle);
-                    }
-                }
-                }),
-                'animationManager.isEnabled': false
-            });
-
-        // shows when hovering over a node
-        let commonToolTip =
-            this.$(go.Adornment, 'Auto',
-            { isShadowed: true },
+              isShadowed: true
+            },
             this.$(go.Shape, { fill: '#FFFFCC' }),
             this.$(go.Panel, 'Vertical',
-                { margin: 3 },
+                {
+                  margin: 3
+                },
                 this.$(go.TextBlock,  // bound to node data
-                { margin: 4, font: 'bold 12pt sans-serif' },
-                new go.Binding('text')),
-                this.$(go.TextBlock,  // bound to node data
-                new go.Binding('text', 'color', function(c) { return 'Color: ' + c; })),
-                this.$(go.TextBlock,  // bound to Adornment because of call to Binding.ofObject
-                new go.Binding('text', '', function(ad) { return 'Connections: ' + ad.adornedPart.linksConnected.count; }).ofObject())
+                  {
+                    margin: 4, font: 'bold 12pt sans-serif'
+                  },
+                  new go.Binding('text')),
+                  this.$(go.TextBlock,  // bound to Adornment because of call to Binding.ofObject
+                new go.Binding('text', '',
+                (ad) => {
+                  return 'Connections: ' + ad.adornedPart.linksConnected.count;
+                }).ofObject())
             )  // end Vertical Panel
             );  // end Adornment
-
-        // define the Node template
-        this.myDiagram.nodeTemplate =
+    this.myDiagram.nodeTemplate =
             this.$(go.Node, 'Spot',
             {
                 locationSpot: go.Spot.Center,
                 locationObjectName: 'SHAPE',  // Node.location is the center of the Shape
                 selectionAdorned: false,
-                // click: this.nodeClicked,
+                click: this.nodeClicked,
                 toolTip: commonToolTip
             },
             this.$(go.Shape, 'Circle',
@@ -162,31 +123,76 @@ export class ExplorativeSearchDetailsComponent implements AfterViewInit, OnChang
                 },
                 new go.Binding('stroke', 'color'))
         );
-        this.generateGraph();
     }
 
     private generateGraph(): void {
         // Current Implementation shows only the figure for Single Query
         // need checks here..
+        let names: string[] = [];
         for (let eachEntry of this.config) {
-            this.names.push(eachEntry['concept']);
+            names.push(eachEntry['concept']);
             for (let conceptName of eachEntry['dataproperties']){
-                this.names.push(conceptName);
+                names.push(conceptName);
             }
         }
-        console.log(this.names);
+        // console.log(names);
 
         let nodeDataArray: any = [];
-        for (let i = 0; i < this.names.length; i++ ) {
-            nodeDataArray.push({key: i, text: this.names[i], color: go.Brush.randomColor(128, 240) });
+        for (let i = 0; i < names.length; i++ ) {
+            nodeDataArray.push({key: i, text: names[i], color: go.Brush.randomColor(128, 240) });
         }
         let linkDataArray: any = [];
         for (let i = 0; i < nodeDataArray.length - 1; i++ ) {
 
             linkDataArray.push({from: 0, to: (i + 1), color: go.Brush.randomColor(0, 128) });
         }
+        this.myDiagram.layout = this.$(RadialLayout, {
+            maxLayers: 2,
+            rotateNode: function(node: any, angle: any, sweep: any, radius: any) {
+                    // rotate the nodes and make sure the text is not upside-down
+                    node.angle = angle;
+                    let label = node.findObject('TEXTBLOCK');
+                    if (label !== null) {
+                    label.angle = ((angle > 90 && angle < 270 || angle < -90) ? 180 : 0);
+                    }
+                },
+                commitLayers: function() {
+                    // optional: add circles in the background
+                    // need to remove any old ones first
+                    let diagram = this.diagram;
+                    let gridlayer = diagram.findLayer('Grid');
+                    let circles = new go.Set(go.Part);
+                    gridlayer.parts.each((circle: any) => {
+                    if (circle.name === 'CIRCLE') { circles.add(circle); }
+                    });
+                    circles.each(function(circle) {
+                    diagram.remove(circle);
+                    });
+                    // add circles centered at the root
+                    let $ = go.GraphObject.make;  // for conciseness in defining templates
+                    for (let lay = 1; lay <= this.maxLayers; lay++) {
+                    let radius = lay * this.layerThickness;
+                    let circle =
+                        $(go.Part,
+                        { name: 'CIRCLE', layerName: 'Grid' },
+                        { locationSpot: go.Spot.Center, location: this.root.location },
+                        $(go.Shape, 'Circle',
+                            { width: radius * 2, height: radius * 2 },
+                            { fill: 'rgba(200,200,200,0.2)', stroke: null }));
+                    diagram.add(circle);
+                    }
+                }
+        });
 
      this.myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
-    }
+     let centerPoint = nodeDataArray[0];
+     let rootNode = this.myDiagram.findNodeForData(centerPoint);
+     let rootDiagram = rootNode.diagram;
+     rootNode.category = 'Root';
+     rootDiagram.layoutDiagram(true);
+    } // ngAfterViewInit ends..
 
+    nodeClicked(e, root): void {
+        console.log(root.data.text);
+    }
 }
