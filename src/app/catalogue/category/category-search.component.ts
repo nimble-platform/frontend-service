39324c50fecdde44bd9cb11fbd7ea8ff1ec4c5dd
Suat Gonul <suat@srdc.com.tr>
2017-12-08 10:18:07 +0300

    properly terminated loading indicator

--- src/app/catalogue/category/category-search.component.ts
+++ src/app/catalogue/category/category-search.component.ts
@@ -98,6 +98,8 @@ export class CategorySearchComponent implements OnInit {
         let userId = this.cookieService.get("user_id");
         this.catalogueService.getCatalogue(userId).then(catalogue => {
             this.router.navigate(['publish'], {queryParams: {pageRef: "category"}});
+        }).catch(() => {
+            this.error_detc = true;
         });
     }
 }
