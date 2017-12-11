a1bf5ae8c6e71d2e35c8957ff839f0c88bcc55bf
Oliver Jung <oliver.jung@salzburgresearch.at>
2017-12-11 09:58:32 +0100

    Prevent excessive rxJs loading, enable debug flag for circumventing role checks

--- src/app/bpe/bp.service.ts
+++ src/app/bpe/bp.service.ts
@@ -1,7 +1,7 @@
 // Imports
 import {Injectable} from '@angular/core';
 import {Http, Response, Headers, RequestOptions} from '@angular/http';
-import {Observable} from 'rxjs/Rx';
+import {Observable} from 'rxjs/Observable';
 import * as configuration from '../globals';
 
 // Import RxJs required methods
