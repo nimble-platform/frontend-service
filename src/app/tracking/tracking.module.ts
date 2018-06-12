11912596d74ac8b9990f832425db4bf440538d61
Shantanoo <shantanoo.desai@gmail.com>
2018-06-12 18:55:19 +0200

    Added Tracking Component to the Platform v1.0
    - This component addresses Issue #92 for introducing
      a tracking and tracing component to the platform
    
    Signed-off-by: Shantanoo <shantanoo.desai@gmail.com>

--- src/app/tracking/tracking.module.ts
+++ src/app/tracking/tracking.module.ts
@@ -8,6 +8,8 @@ import { TrackingRoutingModule } from './tracking-routing.module';
 
 import { TrackingComponent } from './tracking.component';
 import { TrackingFormComponent } from './tracking-form.component';
+import {NgxGraphModule} from '@swimlane/ngx-graph';
+import {NgxChartsModule} from '@swimlane/ngx-charts';
 
 @NgModule({
     imports: [
@@ -17,6 +19,8 @@ import { TrackingFormComponent } from './tracking-form.component';
         ReactiveFormsModule,
         HttpModule,
         TrackingRoutingModule,
+        NgxGraphModule,
+        NgxChartsModule,
         NgbModule.forRoot()
     ],
     declarations: [
