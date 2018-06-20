663dcd432e241f10f59b602dc11fa6e1cb18d7a1
Anthony <ferrando.tony@gmail.com>
2018-06-20 12:49:03 +0200

    Remove dead code

--- src/app/simple-search/simple-search-routing.module.ts
+++ src/app/simple-search/simple-search-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {SimpleSearchComponent} from './simple-search.component';
-import {SimpleSearchDetailsComponent} from './simple-search-details.component';
 
 const routes: Routes = [
-	{path: '', component: SimpleSearchComponent},
-    {path: 'details', component: SimpleSearchDetailsComponent}
+	{path: '', component: SimpleSearchComponent}
 ];
 
 @NgModule({
