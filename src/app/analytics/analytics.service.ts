656aa21ea1a0c11fc936540d1b1db895e897b536
Oliver Jung <oliver.jung@salzburgresearch.at>
2018-07-09 11:45:01 +0200

    Improved platform analytics

--- src/app/analytics/analytics.service.ts
+++ src/app/analytics/analytics.service.ts
@@ -8,8 +8,8 @@ import {CookieService} from 'ng2-cookies';
 export class AnalyticsService {
 
     private headers = new Headers({'Content-Type': 'application/json'});
-    private url_plat = myGlobals.data_aggregation_endpoint;
-    private url_comp = `${myGlobals.bpe_endpoint}/statistics`;
+    private url_da = myGlobals.data_aggregation_endpoint;
+    private url_bpe = `${myGlobals.bpe_endpoint}/statistics`;
 
     constructor(
         private http: Http,
@@ -18,7 +18,7 @@ export class AnalyticsService {
     }
 
     getPlatAnalytics(): Promise<any> {
-  		const url = `${this.url_plat}/`;
+  		const url = `${this.url_da}`;
   		return this.http
   		.get(url, {headers: this.headers})
   		.toPromise()
@@ -26,30 +26,17 @@ export class AnalyticsService {
   		.catch(this.handleError);
   	}
 
-    getTradingVolumeTotal(status:string): Promise<any> {
-      var url = `${this.url_comp}/trading-volume`;
-      if (status != "")
-        url = `${url}?status=${status}`;
-      return this.http
+    getCompAnalytics(comp:string): Promise<any> {
+  		const url = `${this.url_da}?companyID=${comp}`;
+  		return this.http
   		.get(url, {headers: this.headers})
   		.toPromise()
   		.then(res => res.json())
   		.catch(this.handleError);
-    }
-
-    getTradingVolumeComp(comp:string,status:string): Promise<any> {
-      var url = `${this.url_comp}/trading-volume?companyId=${comp}`;
-      if (status != "")
-        url = `${url}&status=${status}`;
-      return this.http
-  		.get(url, {headers: this.headers})
-  		.toPromise()
-  		.then(res => res.json())
-  		.catch(this.handleError);
-    }
+  	}
 
     getNonOrdered(comp:string): Promise<any> {
-      const url = `${this.url_comp}/non-ordered?companyId=${comp}`;
+      const url = `${this.url_bpe}/non-ordered?companyId=${comp}`;
       return this.http
   		.get(url, {headers: this.headers})
   		.toPromise()
--- src/app/analytics/platform-analytics.component.ts
+++ src/app/analytics/platform-analytics.component.ts
@@ -26,7 +26,6 @@ export class PlatformAnalyticsComponent implements OnInit {
     trade_yellow_perc_str = "0%";
     trade_red_perc_str = "0%";
     loading = false;
-    loading2 = false;
 
     constructor(
         private analyticsService: AnalyticsService
@@ -35,61 +34,30 @@ export class PlatformAnalyticsComponent implements OnInit {
 
     ngOnInit(): void {
         this.loading = true;
-        this.loading2 = true;
+        //this.loading2 = true;
         this.analyticsService.getPlatAnalytics()
         .then(res => {
           this.loading = false;
           this.user_count = res.identity.totalUsers;
           this.comp_count = res.identity.totalCompanies;
-          this.bp_count = res.businessProcessCount.total;
+          this.bp_count = Math.round(res.businessProcessCount.state.approved + res.businessProcessCount.state.waiting + res.businessProcessCount.state.denied);
           this.green_perc = Math.round((res.businessProcessCount.state.approved*100)/this.bp_count);
           this.green_perc_str = this.green_perc+"%";
           this.yellow_perc = Math.round((res.businessProcessCount.state.waiting*100)/this.bp_count);
           this.yellow_perc_str = this.yellow_perc+"%";
           this.red_perc = 100 - this.green_perc - this.yellow_perc;
           this.red_perc_str = this.red_perc+"%";
+          this.trade_count = Math.round(res.tradingVolume.approved + res.tradingVolume.waiting + res.tradingVolume.denied);
+          this.trade_green_perc = Math.round((res.tradingVolume.approved*100)/this.trade_count);
+          this.trade_green_perc_str = this.trade_green_perc+"%";
+          this.trade_yellow_perc = Math.round((res.tradingVolume.waiting*100)/this.trade_count);
+          this.trade_yellow_perc_str = this.trade_yellow_perc+"%";
+          this.trade_red_perc = 100 - this.trade_green_perc - this.trade_yellow_perc;
+          this.trade_red_perc_str = this.trade_red_perc+"%";
     		})
         .catch(error => {
           this.loading = false;
   			});
-
-        this.analyticsService.getTradingVolumeTotal("")
-        .then(res => {
-          if (isNaN(res) || res <= 0) {
-            this.loading2 = false;
-          }
-          else {
-            this.trade_count = parseInt(res);
-            this.analyticsService.getTradingVolumeTotal("Approved")
-            .then(res => {
-              if (isNaN(res) || res <= 0) {}
-              else {
-                this.trade_green_perc = Math.round((res*100)/this.trade_count);
-                this.trade_green_perc_str = this.trade_green_perc+"%";
-                this.analyticsService.getTradingVolumeTotal("WaitingResponse")
-                .then(res => {
-                  if (isNaN(res) || res <= 0) {}
-                  else {
-                    this.trade_yellow_perc = Math.round((res*100)/this.trade_count);
-                    this.trade_yellow_perc_str = this.trade_yellow_perc+"%";
-                  }
-                  this.trade_red_perc = 100 - this.trade_green_perc - this.trade_yellow_perc;
-                  this.trade_red_perc_str = this.trade_red_perc+"%";
-                  this.loading2 = false;
-                })
-                .catch(error => {
-                  this.loading2 = false;
-                });
-              }
-            })
-            .catch(error => {
-              this.loading2 = false;
-            });
-          }
-        })
-        .catch(error => {
-          this.loading2 = false;
-        });
 
     }
 
