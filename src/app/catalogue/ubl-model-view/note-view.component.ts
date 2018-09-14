358d5ea324d842f006a72c44b0890daf7561be62
dogukan10 <dogukan10cav@gmail.com>
2018-09-14 15:26:16 +0300

    Better handling of note-view component

--- src/app/catalogue/ubl-model-view/note-view.component.ts
+++ src/app/catalogue/ubl-model-view/note-view.component.ts
@@ -7,7 +7,14 @@ import {Component, Input} from '@angular/core';
 export class NoteViewComponent{
 
     @Input() notes:string[];
+    @Input() requestNotes:string[]; // special case for negotiation response
     @Input() readonly:boolean = true;
+    @Input() label:string;
+
+    @Input() firstCol:string = "col-3";
+    @Input() secondCol:string = null; // negotiation request
+    @Input() thirdCol:string = null; // special case for negotiation response
+    @Input() lastCol:string = "col-9";
 
     onRemoveNote(index){
         this.notes.splice(index,1);
