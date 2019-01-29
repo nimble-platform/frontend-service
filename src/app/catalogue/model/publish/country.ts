630dab8a778535d9554b5911f7b00e427dfcb0fd
dogukanCavdaroglu <dogukan10cav@gmail.com>
2019-01-29 18:24:31 +0300

    Minor adaptations for TextType

--- src/app/catalogue/model/publish/country.ts
+++ src/app/catalogue/model/publish/country.ts
@@ -2,9 +2,10 @@
  * Created by deniz on 16/07/17.
  */
 import {Text} from "./text";
+import {DEFAULT_LANGUAGE} from '../constants';
 
 export class Country {
     constructor(
-        public name: Text = null
+        public name: Text = new Text(null,DEFAULT_LANGUAGE())
     ) {  }
 }
