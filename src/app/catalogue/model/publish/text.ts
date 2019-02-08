ff4ce4512578eba88be4039b84c5e55dfcb5a45c
dogukan10 <dogukan10cav@gmail.com>
2019-02-08 10:13:34 +0300

    set languageID to default language of the browser in the constructor of Text

--- src/app/catalogue/model/publish/text.ts
+++ src/app/catalogue/model/publish/text.ts
@@ -1,6 +1,8 @@
+import {DEFAULT_LANGUAGE} from '../constants';
+
 export class Text {
     constructor(
         public value: string = null,
-        public languageID: string = "en"
+        public languageID: string = DEFAULT_LANGUAGE()
     ) {  }
 }
