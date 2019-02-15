967fcee335c8f18af2373d59231432e136b8de50
dogukanCavdaroglu <dogukan10cav@gmail.com>
2019-02-15 22:25:41 +0300

    Fixed a bug

--- src/app/catalogue/model/publish/catalogue-pagination-response.ts
+++ src/app/catalogue/model/publish/catalogue-pagination-response.ts
@@ -5,6 +5,6 @@ export class CataloguePaginationResponse {
         public catalogueUuid:string = null, // uuid of the catalogue
         public size: number = null, // the number of catalogue lines which the catalogue contains
         public catalogueLines: CatalogueLine[] = null, // catalogue lines of the catalogue
-        public categoryNames: String[] = null // names of the categories which are included in catalogue lines of the catalogue
+        public categoryNames: String[] = [] // names of the categories which are included in catalogue lines of the catalogue
     ) {  }
 }
