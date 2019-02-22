e07d9efd2644e456623c8e6be8c4812546383167
dogukan10 <dogukan10cav@gmail.com>
2019-02-22 13:39:05 +0300

    Proper handling of order price amount

--- src/app/bpe/bp-view/negotiation/negotiation-request.component.ts
+++ src/app/bpe/bp-view/negotiation/negotiation-request.component.ts
@@ -126,6 +126,8 @@ export class NegotiationRequestComponent implements OnInit {
                 this.callStatus.error("Failed to send Terms", error);
             });
         } else {
+            // set the item price, otherwise we will lose item price information
+            this.bpDataService.requestForQuotation.requestForQuotationLine[0].lineItem.price.priceAmount.value = this.wrapper.rfqPriceWrapper.totalPrice/this.wrapper.rfqPriceWrapper.quantity.value;
             // just go to order page
             this.bpDataService.initOrderWithRfq();
             this.bpDataService.proceedNextBpStep("buyer", "Order")
