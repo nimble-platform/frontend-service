83effcfb4ecc8ad8c0ee80cfaa577d862c031719
Akayeshmantha <akayeshmantha@apache.org>
2019-03-12 09:29:32 +0100

    updated phone number as mandatory field in fmp.

--- src/environments/globals.staging.ts
+++ src/environments/globals.staging.ts
@@ -91,6 +91,7 @@ export const config = {
   "imprint": "<u>Platform Owner & Provider</u><br/><b>Salzburg Research Forschungsgesellschaft m.b.H.</b><br/>Jakob Haringer Straße 5/3<br/>5020 Salzburg, Austria<br/>Phone: +43.662.2288.200<br/>Fax: +43.662.2288.222<br/>E-Mail: <a href='mailto:info@salzburgresearch.at'>info@salzburgresearch.at</a><br/>Internet: <a href='https://www.salzburgresearch.at' target='_blank'>www.salzburgresearch.at</a><br/>Managing Director: Siegfried Reich<br/>Registry Number: LG Salzburg (FN 149016 t)<br/>UID: ATU 41145408<br/>Content Officer: Siegfried Reich<br/>Owner: State of Salzburg (100%)",
   "logoPath": "./assets/logo_mvp.png",
   "logoRequired": false,
+  "phoneNumberRequired": false,
   "requiredAgreements": [
     {
       "title":"End-User License Agreement (EULA)",
