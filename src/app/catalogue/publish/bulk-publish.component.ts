ef44873aab74b37b4955a7c0ffa82782d19d370e
Suat Gonul <suat@srdc.com.tr>
2019-03-22 08:50:24 +0300

    upload statuses are shown for multiple templates

--- src/app/catalogue/publish/bulk-publish.component.ts
+++ src/app/catalogue/publish/bulk-publish.component.ts
@@ -4,6 +4,7 @@ import {DEFAULT_LANGUAGE} from "../model/constants";
 import {CategoryService} from "../category/category.service";
 import {CatalogueService} from "../catalogue.service";
 import {CookieService} from "ng2-cookies";
+import {ProductPublishComponent} from "./product-publish.component";
 /**
  * Created by suat on 20-Mar-19.
  */
@@ -16,6 +17,8 @@ export class BulkPublishComponent {
     @Input() selectCategories:Category[];
 
     publishStatus:CallStatus = new CallStatus();
+    uploadPublishStatus: CallStatus[] = [];
+    selectedFileList: FileList;
     showCategoryWarning: boolean = false;
 
     constructor(private categoryService: CategoryService,
@@ -64,27 +67,44 @@ export class BulkPublishComponent {
     }
 
     uploadTemplate(event: any, uploadMode: string) {
-        this.publishStatus.submit();
         let catalogueService = this.catalogueService;
         let userId: string = this.cookieService.get("user_id");
-        let fileList: FileList = event.target.files;
-        if (fileList.length > 0) {
-            let file: File = fileList[0];
+        this.selectedFileList = event.target.files;
+        if (this.selectedFileList.length > 0) {
+            // initialize one call status for each uploaded file
+            this.uploadPublishStatus = [];
+            for(let i=0; i<this.selectedFileList.length; i++) {
+                this.uploadPublishStatus.push(new CallStatus());
+            }
+
             let self = this;
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                // reset the target value so that the same file could be chosen more than once
-                event.target.value = "";
-                catalogueService.uploadTemplate(userId, file, uploadMode).then(res => {
-                        self.publishStatus.callback(null);
-                        ProductPublishComponent.dialogBox = false;
-                        self.router.navigate(['dashboard'], {queryParams: {tab: "CATALOGUE"}});
-                    },
-                    error => {
-                        self.publishStatus.error("Failed to upload the template:  " + error);
-                    });
-            };
-            reader.readAsDataURL(file);
+            let callbackCount: number = 0;
+            for(let i=0; i<this.selectedFileList.length; i++) {
+                this.uploadPublishStatus[i].submit();
+                let file:File = self.selectedFileList[i];
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    catalogueService.uploadTemplate(userId, file, uploadMode).then(res => {
+                            self.uploadPublishStatus[i].callback("Uploaded " + file.name + " successfully");
+                            ProductPublishComponent.dialogBox = false;
+                            self.resetEventWhenUploadCompletes(++callbackCount, self.selectedFileList.length, event);
+                        },
+                        error => {
+                            self.uploadPublishStatus[i].error("Failed to upload: " + file.name + "\n" + error);
+                            self.resetEventWhenUploadCompletes(++callbackCount, self.selectedFileList.length, eventx1);
+                        });
+
+                    // reset the event target
+                    callbackCount++;
+                };
+                reader.readAsDataURL(self.selectedFileList[i]);
+            }
+        }
+    }
+
+    private resetEventWhenUploadCompletes(currentCount: int, totalCount, event: any): void {
+        if(currentCount == totalCount) {
+            event.target.value = "";
         }
     }
 
