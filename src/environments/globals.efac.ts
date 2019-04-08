d426f1058460275f2d0f64d7e7589da7d8b41f06
Akayeshmantha <akayeshmantha@apache.org>
2019-04-08 16:17:09 +0200

    final efac changes.

--- src/environments/globals.efac.ts
+++ src/environments/globals.efac.ts
@@ -94,18 +94,14 @@ export const config = {
     }
   },
   "dataChannelsEnabled" : false,
-  "imprint": "<table class='table table-borderless'><tr><td class='w-50 p-0 pr-3'><u>Platform Owner</u><br/><b>AIDIMME - Technological Institute of Metalworking, Furniture, Wood, Packaging and Related sectors</b><br/>Technological Park, Benjamín Franklin Street 13<br/>46980 Paterna (Valencia), Spain<br/>Phone: +34.961.366.070<br/>E-Mail: <a href='mailto:info@aidimme.es'>info@aidimme.es</a><br/>CIF: G46261590</td><td class='w-50 p-0 pl-3'><u>Platform Provider</u><br/><b>Salzburg Research Forschungsgesellschaft m.b.H.</b><br/>Jakob Haringer Straße 5/3<br/>5020 Salzburg, Austria<br/>Phone: +43.662.2288.200<br/>Fax: +43.662.2288.222<br/>E-Mail: <a href='mailto:info@salzburgresearch.at'>info@salzburgresearch.at</a><br/>Internet: <a href='https://www.salzburgresearch.at' target='_blank'>www.salzburgresearch.at</a><br/>Managing Director: Siegfried Reich<br/>Registry Number: LG Salzburg (FN 149016 t)<br/>UID: ATU 41145408<br/>Content Officer: Siegfried Reich<br/>Owner: State of Salzburg (100%)</td></tr></table>",
-  "logoPath": "./assets/logo_fmp.png",
+  "imprint": "<u>Platform Owner & Provider</u><br/><b>Salzburg Research Forschungsgesellschaft m.b.H.</b><br/>Jakob Haringer Straße 5/3<br/>5020 Salzburg, Austria<br/>Phone: +43.662.2288.200<br/>Fax: +43.662.2288.222<br/>E-Mail: <a href='mailto:info@salzburgresearch.at'>info@salzburgresearch.at</a><br/>Internet: <a href='https://www.salzburgresearch.at' target='_blank'>www.salzburgresearch.at</a><br/>Managing Director: Siegfried Reich<br/>Registry Number: LG Salzburg (FN 149016 t)<br/>UID: ATU 41145408<br/>Content Officer: Siegfried Reich<br/>Owner: State of Salzburg (100%)",
+  "logoPath": "./assets/logo_efac.png",
   "logoRequired": true,
   "phoneNumberRequired": true,
   "requiredAgreements": [
     {
-      "title":"Privacy Policy",
-      "src":"./assets/privacy_policy.pdf"
-    },
-    {
-      "title":"Terms of Service (ToS)",
-      "src":"./assets/tos.pdf"
+      "title":"End-User License Agreement (EULA)",
+      "src":"./assets/eula.pdf"
     }
   ],
   "showCompanyMembers": true,
