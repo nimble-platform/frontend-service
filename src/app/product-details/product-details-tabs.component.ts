d73c6b76ad67288aec1f30545752516d6b262c1f
Akayeshmantha <akayeshmantha@apache.org>
2019-04-09 18:40:32 +0200

    Fixes to hiding tabs.

--- src/app/product-details/product-details-tabs.component.ts
+++ src/app/product-details/product-details-tabs.component.ts
@@ -2,10 +2,12 @@ import { Component, Input, OnInit } from "@angular/core";
 import { ProductDetailsTab } from "./model/product-details-tab";
 import { ProductWrapper } from "../common/product-wrapper";
 import { BpWorkflowOptions } from "../bpe/model/bp-workflow-options";
+import { BPEService } from "../bpe/bpe.service";
 import { ItemProperty } from "../catalogue/model/publish/item-property";
 import { getPropertyValuesAsStrings } from "../common/utils";
 import { CompanySettings } from "../user-mgmt/model/company-settings";
 import * as myGlobals from '../globals';
+import { CallStatus } from "../common/call-status";
 
 @Component({
     selector: 'product-details-tabs',
@@ -25,14 +27,41 @@ export class ProductDetailsTabsComponent implements OnInit {
     selectedTab: ProductDetailsTab;
 
     isLogistics: boolean = false;
+    haveDatails: boolean = true;
+    haveTransportServiceDetails = true;
+    haveCertificates = true;
+    haveLcps = true;
+    haveRating = true;
 
-    constructor() {
+    initCallStatus: CallStatus = new CallStatus();
 
-    }
+    constructor(
+        private bpeService: BPEService,
+    ) {}
 
     ngOnInit() {
         this.selectedTab = this.showOverview ? "OVERVIEW" : "DETAILS";
-        this.isLogistics = this.wrapper.getLogisticsStatus();
+        this.isLogistics = this.wrapper.getLogisticsStatus();        
+        if(this.wrapper.getDimensions().length == 0 && this.wrapper.getUniquePropertiesWithValue().length == 0){
+            this.selectedTab = "DELIVERY_TRADING";
+            this.haveDatails = false;
+        }
+        if(this.settings.certificates.length == 0 && this.wrapper.line.goodsItem.item.certificate.length == 0){
+            this.haveCertificates = false;
+        }   
+        if(this.wrapper.line.goodsItem.item.lifeCyclePerformanceAssessmentDetails == null){
+            this.haveLcps = false;
+        }
+        this.bpeService.getRatingsSummary(this.settings.companyID).then(ratings => {
+            this.initCallStatus.submit();
+            if (ratings.totalNumberOfRatings <= 0) {
+                this.haveRating = false;
+            }
+            this.initCallStatus.callback("Ratings successfully fetched", true);
+          })
+          .catch(error => {
+            this.initCallStatus.error("Error while fetching company ratings", error);
+          });
     }
 
     onSelectTab(event: any): void {
@@ -46,5 +75,32 @@ export class ProductDetailsTabsComponent implements OnInit {
 
     getHumanReadablePropertyName(propertyName:string): string{
         return propertyName.replace("Has", "");
+    }
+
+    getStatusOfTansportTab(data){
+        if(data){
+            this.selectedTab = "CERTIFICATES";
+            this.haveTransportServiceDetails = false;
+        }
+    }
+
+    getTrustStatusTab(data){
+        if(data){
+            this.selectedTab = "LCPA";
+            this.haveCertificates = false;
+        }
+    }
+
+    getlcpaStatusTab(data){
+        if(data){
+            this.selectedTab = "COMPANY";
+            this.haveLcps = false;
+        }
+    }
+
+    getRatingStatus(data){
+        if(data){
+            this.haveRating = false;
+        }
     }
 }
