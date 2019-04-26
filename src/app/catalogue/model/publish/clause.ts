32f8fdb4d53ec8e73e12eb48832a774f5671b846
dogukan10 <dogukan10cav@gmail.com>
2019-04-26 09:29:43 +0300

    updated Clause and Contract models

--- src/app/catalogue/model/publish/clause.ts
+++ src/app/catalogue/model/publish/clause.ts
@@ -1,6 +1,9 @@
+import {Text} from './text';
+
 export class Clause {
     constructor(
         public id:string = null,
-        public type:string = null
+        public type:string = null,
+        public content:Text[] = []
     ) {  }
 }
