e97807f446524e4b422a64cfd08deb22f5a87d4f
dogukan10 <dogukan10cav@gmail.com>
2019-07-10 09:43:51 +0300

    do not show Dispatch Order and Search For Transport Service Provider buttons if Fulfilment is not included in the seller's workflow

--- src/app/bpe/bp-view/order/order.component.ts
+++ src/app/bpe/bp-view/order/order.component.ts
@@ -73,6 +73,9 @@ export class OrderComponent implements OnInit {
 
     showPurchaseOrder:boolean = false;
 
+    // map representing the workflow of seller's company
+    companyWorkflowMap = null;
+
     constructor(public bpDataService: BPDataService,
                 private userService: UserService,
                 private bpeService: BPEService,
@@ -106,6 +109,8 @@ export class OrderComponent implements OnInit {
             this.bpDataService.getCatalogueLine().requiredItemLocationQuantity.applicableTaxCategory[0].percent,
             this.order.orderLine[0].lineItem.quantity
         );
+
+        this.companyWorkflowMap = this.bpDataService.getCompanyWorkflowMap();
 
         // null check is for checking whether a new order is initialized
         // preceding process id check is for checking whether there is any preceding process before the order
