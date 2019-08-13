1953e56ca7f200b9abbf9f4362435783d9b9dbf5
Shantanoo <shantanoo.desai@gmail.com>
2019-08-13 16:56:53 +0200

    Add FactorsComponent for QualiExplore
    - Add the v1.0 of Factors rendering for QualiExplore
    - Based on input from FiltersComponent necessary UI
      is adapted for displaying Factors

--- src/app/qualiexplore/factors/factors.component.ts
+++ src/app/qualiexplore/factors/factors.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FactorsService } from './factors.service';
+import { TreeviewConfig, TreeviewItem, TreeItem } from 'ngx-treeview';
 
 @Component({
     selector: 'factors',
@@ -8,4 +10,96 @@ import { FactorsService } from './factors.service';
     providers: [ FactorsService ]
 })
 
-export class FactorsComponent {}
+export class FactorsComponent implements OnInit {
+    config = TreeviewConfig.create({
+        hasAllCheckBox: false,
+        hasFilter: false,
+        hasCollapseExpand: false,
+        decoupleChildFromParent: false,
+        maxHeight: 750
+      });
+      items: TreeviewItem[];
+      value: any;
+      valueName = '';
+      selectedFactor: TreeItem;
+      selected: number[] = [];
+      totalHighlightedFactors = 0;
+      totalResolvedFactors = 0;
+      proceedButtonDisabled = false;
+
+    constructor(private service: FactorsService, private route: ActivatedRoute, private router: Router) {}
+
+    ngOnInit() {
+        this.route.queryParams.subscribe(params => {
+            this.selected = JSON.parse(params.ids);
+            // console.log(this.selected);
+            this.service.getFactors()
+            .then((items) => {
+              this.items = this.parseTree([new TreeviewItem(items)]);
+              this.countHighlightedFactors(this.items);
+            });
+          });
+    }
+
+    select(item: TreeItem) {
+        // console.log(item);
+        this.selectedFactor = item;
+        this.proceedButtonDisabled = false;
+      }
+
+      markRead(selectedFactor: TreeItem) {
+        selectedFactor.checked = true;
+        this.totalResolvedFactors++
+      }
+
+      isUrl(link: string): boolean {
+        // console.log(link);
+        const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i');
+        // console.log(pattern.test(link));
+        return !!pattern.test(link);
+      }
+
+      countHighlightedFactors(factors: TreeviewItem[]) {
+        factors.forEach(factor => {
+          if (factor.value !== null) {
+            if (factor.value.highlighted) {
+              this.totalHighlightedFactors++;
+            }
+          }
+          if (factor.children !== undefined) {
+            this.countHighlightedFactors(factor.children);
+          }
+        })
+      }
+
+      parseTree(factors: TreeviewItem[]): TreeviewItem[] {
+        factors.forEach(factor => {
+          // console.log(factor);
+          if (factor.value !== null) {
+            let labels: number[] = factor.value['label_ids'];
+            labels.forEach(label => {
+              if (this.selected.findIndex(l => l === label) > -1) {
+                factor.value['highlighted'] = true;
+                factor.value['class'] = 'fas fa-flag';
+              }
+            });
+          }
+          if (factor.children !== undefined) {
+            this.parseTree(factor.children);
+          }
+
+        });
+        return factors;
+    }
+
+    // WIP
+    // backToStep1() {
+    //     this.router.navigate(['qualiexplore/filters']);
+    // }
+
+}
--- src/app/qualiexplore/factors/factors.service.ts
+++ src/app/qualiexplore/factors/factors.service.ts
@@ -1,5 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { TreeviewItem } from 'ngx-treeview';
 
 @Injectable()
 
-export class FactorsService {}
+export class FactorsService {
+    private _factorsUrl = 'https://gist.githubusercontent.com/shantanoo-desai/5163182aba74baf7ec04d7ac426bd944/raw/Data.json';
+
+    constructor(private http: Http) {}
+
+    getFactors() {
+        return this.http.get(this._factorsUrl)
+            .toPromise()
+            .then(response => response.json())
+            .catch(err => console.log(err));
+    }
+}
