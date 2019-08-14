47430080323392ef1acae292c10635bd7d45552d
Shantanoo <shantanoo.desai@gmail.com>
2019-08-14 17:44:17 +0200

    Add Logic to display filters from Step-1 in UI
    - Using `sessionStorage` values from Step-1 display values that were
       selected in Step - 2
    - Provide collapsible option in UI to show/hide Selected Filters

--- src/app/qualiexplore/factors/factors.component.ts
+++ src/app/qualiexplore/factors/factors.component.ts
@@ -26,80 +26,114 @@ export class FactorsComponent implements OnInit {
       totalHighlightedFactors = 0;
       totalResolvedFactors = 0;
       proceedButtonDisabled = false;
+      selectedFilterDetails = [];
+      collapseSelectedFilters = true;
 
     constructor(private service: FactorsService, private route: ActivatedRoute, private router: Router) {}
 
     ngOnInit() {
         this.route.queryParams.subscribe(params => {
             this.selected = JSON.parse(params.ids);
-            // console.log(this.selected);
-            this.service.getFactors()
-            .then((items) => {
-              this.items = this.parseTree([new TreeviewItem(items)]);
-              this.countHighlightedFactors(this.items);
+
+            // Display Logic to show selected filters from Step - 1
+            // as opposed to IDs
+            this.selected.forEach(id => {
+              const currentFiltersFromStep1: object[] = JSON.parse(sessionStorage.getItem('currentFilters'));
+              currentFiltersFromStep1.forEach(filter => {
+                filter['labels'].forEach(label => {
+                  if (label['id'] === id) {
+                    this.selectedFilterDetails.push({name: label['name'], parent: filter['name']});
+                  }
+                });
+              });
             });
+              this.service.getFactors()
+              .then((items) => {
+                this.items = this.parseTree([new TreeviewItem(items)]);
+                this.countHighlightedFactors(this.items);
+              });
           });
     }
 
+    /**
+     * Selected Factor information
+     * @param item selected factor from the ngx-treeview
+     */
     select(item: TreeItem) {
         // console.log(item);
         this.selectedFactor = item;
         this.proceedButtonDisabled = false;
-      }
-
-      markRead(selectedFactor: TreeItem) {
-        selectedFactor.checked = true;
-        this.totalResolvedFactors++
-      }
-
-      isUrl(link: string): boolean {
-        // console.log(link);
-        const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$', 'i');
-        // console.log(pattern.test(link));
-        return !!pattern.test(link);
-      }
-
-      countHighlightedFactors(factors: TreeviewItem[]) {
-        factors.forEach(factor => {
-          if (factor.value !== null) {
-            if (factor.value.highlighted) {
-              this.totalHighlightedFactors++;
-            }
-          }
-          if (factor.children !== undefined) {
-            this.countHighlightedFactors(factor.children);
-          }
-        })
-      }
-
-      parseTree(factors: TreeviewItem[]): TreeviewItem[] {
-        factors.forEach(factor => {
-          // console.log(factor);
-          if (factor.value !== null) {
-            let labels: number[] = factor.value['label_ids'];
-            labels.forEach(label => {
-              if (this.selected.findIndex(l => l === label) > -1) {
-                factor.value['highlighted'] = true;
-                factor.value['class'] = 'fas fa-flag';
-              }
-            });
-          }
-          if (factor.children !== undefined) {
-            this.parseTree(factor.children);
-          }
-
-        });
-        return factors;
     }
 
-    // WIP
-    // backToStep1() {
-    //     this.router.navigate(['qualiexplore/filters']);
-    // }
+    /**
+     * When user presses the Proceed button in the Card (right-side)
+     * @param selectedFactor The selected factor from `select`
+     */
+    markRead(selectedFactor: TreeItem) {
+      selectedFactor.checked = true;
+      this.totalResolvedFactors++;
+      // console.log(this.items);
+    }
+
+    /**
+     * RegEx check if the link string is a URL or DOI
+     * @param link selected factor's array of string under `sources` key
+     */
+    isUrl(link: string): boolean {
+      const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$', 'i');
+      // console.log(pattern.test(link));
+      return !!pattern.test(link);
+    }
+
+    /**
+     * Recursive Function to calculate Highlighted factors based on IDs from Step-1
+     * @param factors incoming information for ngx-treeview
+     */
+    countHighlightedFactors(factors: TreeviewItem[]) {
+      factors.forEach(factor => {
+        if (factor.value !== null) {
+          if (factor.value.highlighted) {
+            this.totalHighlightedFactors++;
+          }
+        }
+        if (factor.children !== undefined) {
+          this.countHighlightedFactors(factor.children);
+        }
+      })
+    }
+
+    /**
+     * Recursion Function to highlight relevant factors based on IDs from Step-1
+     * @param factors Response from API. Recursive JSON
+     */
+    parseTree(factors: TreeviewItem[]): TreeviewItem[] {
+      factors.forEach(factor => {
+        if (factor.value !== null) {
+          let labels: number[] = factor.value['label_ids'];
+          labels.forEach(label => {
+            if (this.selected.findIndex(l => l === label) > -1) {
+              factor.value['highlighted'] = true;
+              factor.value['class'] = 'fas fa-flag';
+            }
+          });
+        }
+        if (factor.children !== undefined) {
+          this.parseTree(factor.children);
+        }
+      });
+      return factors;
+  }
+
+  /**
+   * Navigate back to Step-1 if new filters needed
+   */
+  backToStep1() {
+      this.router.navigate(['qualiexplore/filters']);
+  }
 
 }
