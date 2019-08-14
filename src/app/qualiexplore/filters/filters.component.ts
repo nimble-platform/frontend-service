39bef6a0b305632d63d54e83fe55811e42f22ba1
Shantanoo <shantanoo.desai@gmail.com>
2019-08-14 17:32:50 +0200

    Store selected filter in order to return back
    - Store Selected Filters into `sessionStorage` before proceeding
      to Step - 2
    - Check if previous interaction occured and restore those filters resp.
    - Add Reset Logic

--- src/app/qualiexplore/filters/filters.component.ts
+++ src/app/qualiexplore/filters/filters.component.ts
@@ -17,22 +17,58 @@ export class FiltersComponent implements OnInit {
     constructor(private service: FiltersService, private router: Router) {}
 
     ngOnInit() {
-        this.showFilters();
+        // Get previously selected Filters and Selection Array
+        const previousFilterSelections = sessionStorage.getItem('currentFilters');
+        const previousSelectionsSet = sessionStorage.getItem('currentSelectionsSet');
+        if ( previousFilterSelections !== null && previousSelectionsSet !== null ) {
+            // if Previously interacted then use those values
+            this.filters = JSON.parse(previousFilterSelections);
+            this.selections = JSON.parse(previousSelectionsSet);
+            this._selectionsSet = new Set<number>(this.selections);
+        } else {
+            // call the API
+            this.showFilters();
+        }
     }
 
+    /**
+     * API call to Filter Static JSON
+     */
     showFilters() {
         this.service.getQuestions().then((data: any) => {
             this.filters = data.categories;
         });
     }
 
+    /**
+     * Add the Selection to a Set if checked/ Remove the Selection from a Set if unchecked
+     * @param id number
+     * @param event Event Trigger from HTML Input Element
+     */
     changeCheck(id: number, event: any) {
         (event.target.checked) ? this._selectionsSet.add(id) : this._selectionsSet.delete(id);
         this.selections = Array.from(this._selectionsSet);
     }
 
+    /**
+     * Proceed to Step-2 : Factors
+     * Store the present state of selected filters in sessionStorage
+     */
     proceed() {
+        // store the current filters to localStorage
+        sessionStorage.setItem('currentFilters', JSON.stringify(this.filters));
+        sessionStorage.setItem('currentSelectionsSet', JSON.stringify(this.selections));
         this.router.navigate(['qualiexplore/factors'], {queryParams: {ids: JSON.stringify(this.selections)}});
 
+    }
+
+    /**
+     * Reset All Filters and previously stored Filters in `sessionStorage`
+     */
+    reset() {
+        sessionStorage.clear();
+        this.selections = [];
+        this._selectionsSet.clear();
+        this.showFilters();
     }
 }
