1c6099e30a1863f8e74456bf4341698a2fde2c3b
Akayeshmantha <akayeshmantha@apache.org>
2019-08-14 14:10:26 +0200

    fix analytics module translation.

--- src/app/analytics/analytics.module.ts
+++ src/app/analytics/analytics.module.ts
@@ -13,6 +13,14 @@ import {CompanyManagementComponent} from './company-management.component';
 import {PlatformInfoComponent} from './platform-info.component';
 import {MembersComponent} from './members.component';
 import {ChatComponent} from './chat.component';
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
     imports: [
@@ -22,7 +30,14 @@ import {ChatComponent} from './chat.component';
         HttpModule,
         ReactiveFormsModule,
         AnalyticsRoutingModule,
-        NgbModule.forRoot()
+        NgbModule.forRoot(),
+        TranslateModule.forRoot({
+            loader: {
+              provide: TranslateLoader,
+              useFactory: HttpLoaderFactory,
+              deps: [HttpClient]
+            }
+        })
     ],
     declarations: [
         PlatformAnalyticsComponent,
