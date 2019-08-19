e36164ff890ed3ac5083604b2369180530975b00
Shantanoo <shantanoo.desai@gmail.com>
2019-08-19 16:12:08 +0200

    [TnT][WIP] Add Translation Service for TnT
    - Add Translation for Tracking and Tracing Component on Frontend
    
    - [WIP] Longer sentences are not rendered

--- src/app/tnt/tnt.module.ts
+++ src/app/tnt/tnt.module.ts
@@ -11,6 +11,15 @@ import { TnTFormComponent } from './tnt-form.component';
 import {NgxGraphModule} from '@swimlane/ngx-graph';
 import {NgxChartsModule} from '@swimlane/ngx-charts';
 
+// Translation
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
     imports: [
         CommonModule,
@@ -21,7 +30,14 @@ import {NgxChartsModule} from '@swimlane/ngx-charts';
         TnTRoutingModule,
         NgxGraphModule,
         NgxChartsModule,
-        NgbModule.forRoot()
+        NgbModule.forRoot(),
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
     ],
     declarations: [
       TnTComponent,
