c98877eb6e140248c903190b1a900b128199b608
Suat Gonul <suat@srdc.com.tr>
2019-09-20 18:46:57 +0300

    initial panel to show aggregations of associated products in unshipped orders panel

--- src/app/dashboard/unshipped-orders-tab.component.ts
+++ src/app/dashboard/unshipped-orders-tab.component.ts
@@ -7,6 +7,10 @@ import {TranslateService} from '@ngx-translate/core';
 import {DocumentService} from '../bpe/bp-view/document-service';
 import {Order} from '../catalogue/model/publish/order';
 import {quantityToString, selectPartyName, selectPreferredValue} from '../common/utils';
+import {Item} from "../catalogue/model/publish/item";
+import {ItemProperty} from "../catalogue/model/publish/item-property";
+import {CatalogueLine} from "../catalogue/model/publish/catalogue-line";
+import {Quantity} from "../catalogue/model/publish/quantity";
 
 /**
  * Created by suat on 19-Sep-19.
@@ -20,8 +24,16 @@ export class UnshippedOrdersTabComponent implements OnInit {
     allOrderIds: string[] = [];
     displayedOrderIds: string[] = [];
     orders: Map<string, Order> = new Map<string, Order>();
+    associatedProducts: Map<number, CatalogueLine> = new Map<number, CatalogueLine>();
+    // keeps the aggregated products referred by the orders
+    // key is composed of: product.hjid + unit
+    associatedProductAggregates: Map<string, Quantity> = new Map<string, Quantity>();
+    allOrdersCallStatus: CallStatus = new CallStatus();
     orderIdsCallStatus: CallStatus = new CallStatus();
     orderCallStatuses: Map<string, CallStatus> = new Map<string, CallStatus>();
+    showUnshippedOrders = true;
+    // keeps the error messages, if any, received when getting all orders
+    failedOrderMessages: string[] = [];
 
     pageNum = 0;
     pageSize = 10;
@@ -47,6 +59,7 @@ export class UnshippedOrdersTabComponent implements OnInit {
         this.orderIdsCallStatus.submit();
         this.bpeService.getUnshippedOrderIds(partyId).then(orderIds => {
             this.allOrderIds = orderIds;
+            this.retrieveAllOrders();
             // set timeout is added in order to trigger a page change event of the navigation component
             setTimeout(() => this.pageNum = 1);
 
@@ -54,6 +67,33 @@ export class UnshippedOrdersTabComponent implements OnInit {
 
         }).catch(error => {
             this.orderIdsCallStatus.error('Failed to retrieve unshipped order ids');
+        });
+    }
+
+    retrieveAllOrders(): void {
+        this.failedOrderMessages = [];
+
+        let promises: Promise<any>[] = [];
+        for (let i = 0; i < this.allOrderIds.length; i++) {
+            let orderRetrievalPromise: Promise<any> = this.documentService.getCachedDocument(this.allOrderIds[i]);
+            promises.push(orderRetrievalPromise);
+        }
+
+        this.allOrdersCallStatus.submit();
+        Promise.all(promises
+            .map(promise => promise.catch(error => {
+                this.failedOrderMessages.push(error);
+            }))
+        ).then(orders => {
+            if (this.failedOrderMessages.length > 0) {
+                this.allOrdersCallStatus.error('Failed to retrieve some of the orders.')
+            } else {
+                for (let order of orders) {
+                    this.orders.set(order.id, order);
+                }
+                this.aggregateAssociatedProducts();
+                this.allOrdersCallStatus.callback(null, true);
+            }
         });
     }
 
@@ -80,6 +120,17 @@ export class UnshippedOrdersTabComponent implements OnInit {
             }).catch(error => {
                 callStatus.error('Failed to retrieve order: ' + this.displayedOrderIds[i]);
             });
+        }
+    }
+
+    aggregateAssociatedProducts(): void {
+        for (let order of Array.from(this.orders.values())) {
+            let orderedProductProperties: ItemProperty[] = order.orderLine[0].lineItem.item.additionalItemProperty;
+            for (let itemProperty of orderedProductProperties) {
+                if (itemProperty.associatedCatalogueLineID.length > 0) {
+                    console.log(itemProperty.value);
+                }
+            }
         }
     }
 
