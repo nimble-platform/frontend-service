54600b65824a194abacdee9c8e93f00066604d1b
Suat Gonul <suat@srdc.com.tr>
2019-09-20 09:57:35 +0300

    added pagination to unshipped orders

--- src/app/dashboard/unshipped-orders-tab.component.ts
+++ src/app/dashboard/unshipped-orders-tab.component.ts
@@ -17,11 +17,16 @@ import {quantityToString, selectPartyName, selectPreferredValue} from '../common
 })
 export class UnshippedOrdersTabComponent implements OnInit {
 
-    orderIds: string[] = [];
+    allOrderIds: string[] = [];
+    displayedOrderIds: string[] = [];
     orders: Map<string, Order> = new Map<string, Order>();
     orderIdsCallStatus: CallStatus = new CallStatus();
     orderCallStatuses: Map<string, CallStatus> = new Map<string, CallStatus>();
 
+    pageNum = 0;
+    pageSize = 10;
+
+    // utility methods
     selectPartyName = selectPartyName;
     selectLangLabelFromTextArray = selectPreferredValue;
     quantityToString = quantityToString;
@@ -41,26 +46,41 @@ export class UnshippedOrdersTabComponent implements OnInit {
         let partyId = this.cookieService.get('company_id');
         this.orderIdsCallStatus.submit();
         this.bpeService.getUnshippedOrderIds(partyId).then(orderIds => {
-            this.orderIds = orderIds;
-            for (let i = 0; i < orderIds.length; i++) {
-                let callStatus: CallStatus = new CallStatus();
-                callStatus.submit();
-
-                this.orderCallStatuses.set(orderIds[i], callStatus);
-                this.documentService.getCachedDocument(orderIds[i]).then(order => {
-                    this.orders.set(orderIds[i], order);
-                    callStatus.callback('', true);
-
-                }).catch(error => {
-                    callStatus.error('Failed to retrieve order: ' + orderIds[i]);
-                });
-            }
+            this.allOrderIds = orderIds;
+            // set timeout is added in order to trigger a page change event of the navigation component
+            setTimeout(() => this.pageNum = 1);
 
             this.orderIdsCallStatus.callback(null, true);
 
         }).catch(error => {
             this.orderIdsCallStatus.error('Failed to retrieve unshipped order ids');
         });
+    }
+
+    onUnshippedOrdersPageChange(): void {
+        this.displayedOrderIds = [];
+        let offset: number = (this.pageNum - 1) * this.pageSize;
+        for (let i = offset; i < offset + this.pageSize && i < this.allOrderIds.length; i++) {
+            this.displayedOrderIds.push(this.allOrderIds[i]);
+        }
+
+        for (let i = 0; i < this.displayedOrderIds.length; i++) {
+            if (this.orders.has(this.displayedOrderIds[i])) {
+                continue;
+            }
+
+            let callStatus: CallStatus = new CallStatus();
+            callStatus.submit();
+
+            this.orderCallStatuses.set(this.displayedOrderIds[i], callStatus);
+            this.documentService.getCachedDocument(this.displayedOrderIds[i]).then(order => {
+                this.orders.set(this.displayedOrderIds[i], order);
+                callStatus.callback('', true);
+
+            }).catch(error => {
+                callStatus.error('Failed to retrieve order: ' + this.displayedOrderIds[i]);
+            });
+        }
     }
 
     // navigateToProductDetails(frameContract: DigitalAgreement): void {
