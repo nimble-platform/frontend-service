e127a1758068605425abe695682e852609d3efb9
Shantanoo <shantanoo.desai@gmail.com>
2019-11-07 21:33:39 +0100

    Update data between Event Data & Details Component
    - Instead of sending just the selected event,
      send the present and previous event info.

--- src/app/tnt/tnt-event-data.component.ts
+++ src/app/tnt/tnt-event-data.component.ts
@@ -14,17 +14,30 @@ export class TnTEventDataComponent implements OnChanges {
     pageSize = 5;
     verified: boolean;
     selectedEvent = {};
+    eventsToDescribe: TrackInfo[] = [];
 
     ngOnChanges() {
         if (!this.incomingTrackingInfo.length) {
             return;
         }
         this.selectedEvent = {};
+        this.eventsToDescribe = [];
     }
 
     get events(): TrackInfo[] {
         const everyEvent = this.incomingTrackingInfo;
         this.collectionSize = everyEvent.length;
         return everyEvent.slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+    }
+
+    eventSelection(event: TrackInfo, presentIndex: number) {
+        this.eventsToDescribe = []; // reset on every button press
+        this.selectedEvent = event; // highlight the event
+        if (presentIndex < this.incomingTrackingInfo.length - 1) {
+            this.eventsToDescribe.push(this.incomingTrackingInfo[presentIndex]); // push the selected Event
+            this.eventsToDescribe.push(this.incomingTrackingInfo[presentIndex + 1]); // push the previously occured event
+        } else {
+            console.log('The First ever Event in the chain');
+        }
     }
 }
