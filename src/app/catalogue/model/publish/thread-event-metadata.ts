db782a439773e942c1ff69d5f50e1f3f28ea0ea6
dogukan10 <dogukan10cav@gmail.com>
2020-03-02 13:10:24 +0300

    resolved NIMBLEFMP-1273

--- src/app/catalogue/model/publish/thread-event-metadata.ts
+++ src/app/catalogue/model/publish/thread-event-metadata.ts
@@ -21,6 +21,9 @@ export class ThreadEventMetadata {
         public collaborationStatus: string,
         public sellerFederationId:string,
         public cancellationReason:string,
+        public requestDate:string,
+        public responseDate:string,
+        public completionDate:string,
         public isBeingUpdated: boolean = false, // It's true only while the process instance is being updated.
         public status?: ThreadEventStatus,
         public statusText?: string,
