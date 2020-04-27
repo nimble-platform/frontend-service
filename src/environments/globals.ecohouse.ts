798c02473b886af8a4de45f678c5bad2e4360ad5
Shantanoo <shantanoo.desai@gmail.com>
2020-04-27 12:27:43 +0200

    chore(eco-house): enable delegation

--- src/environments/globals.ecohouse.ts
+++ src/environments/globals.ecohouse.ts
@@ -83,7 +83,7 @@ export const config = {
     "defaultBusinessProcessIds": [
     ],
     "defaultSearchIndex": "Name",
-    "delegationEnabled": false,
+    "delegationEnabled": true,
     "docLink": "https://www.nimble-project.org/docs/",
     "frameContractTabEnabled": true,
     "imprint": "<table class='table table-borderless'><tr><td class='w-50 p-0 pr-3'><u>Platform Owner</u><br/><b>Lindbäcks Bygg Aktiebolag</b><br/>Hammarvägen 21<br/>94336 Öjebyn<br/>Sweden<br/></td><td class='w-50 p-0 pl-3'><u>Platform Provider</u><br/><b>Universität Bremen</b><br/>Bibliothekstraße 1<br/>28359 Bremen, Germany<br/>Phone: +49 421 218-1<br/>Internet: <a href='https://www.uni-bremen.de' target='_blank'>www.uni-bremen.de</a><br/>Die Universität Bremen ist eine Körperschaft des Öffentlichen Rechts. Sie wird durch den Rektor Prof. Dr.-Ing. Bernd Scholz-Reiter gesetzlich vertreten.\n" +
