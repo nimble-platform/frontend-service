f93ed9f779c45df411f2a7243878d63cd4215b78
Mathias Schmoigl <mathias.schmoigl@salzburgresearch.at>
2020-05-01 22:39:47 +0200

    Asset Detail now features empty streaming data view of properties; refactoring of Asset Detail apperance

--- src/app/catalogue/iasset-registry/asset-detail.component.ts
+++ src/app/catalogue/iasset-registry/asset-detail.component.ts
@@ -22,6 +22,9 @@ export class AssetDetail implements OnInit {
     // TESTING - use real asset as soon as solr-search is working ---
     private instance: ModelAssetInstance = null;
     private type : ModelAssetType = new ModelAssetType("", "", "", "", "", null);
+
+    private propertyNames : string[] = [];
+    private selectedPropertyStream: string = "";
     // --------------------------------------------------------------
 
     //-------------------------------------------------------------------------------------
@@ -39,19 +42,26 @@ export class AssetDetail implements OnInit {
         // --------------------------------------------------------------
         this.registryService.getAllAssetInstances("12345")
             .then(instances => {
-
                 this.instance = instances[0];
-
 
                 if(this.instance.assetType != null)
                 {
                     this.registryService.getAssociatedTypeByName(this.instance.assetType)
                         .then(assoctype => {
                             this.type = assoctype;
+                            this.propertyNames = this.type.properties.map( item => item.name ).sort();
+                            this.selectedPropertyStream = this.propertyNames[0];
                     });
                 }
             });
         // --------------------------------------------------------------
+    }
+
+    //-------------------------------------------------------------------------------------
+    // handleSelectedPropertyChange
+    //-------------------------------------------------------------------------------------
+    handleSelectedPropertyChange(event): void {
+        // TODO
     }
 
     //-------------------------------------------------------------------------------------
