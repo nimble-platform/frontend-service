4a3037bae2996bbaf5085fdd9088d4cb52386f47
Oliver Jung <oliver.jung@salzburgresearch.at>
2020-05-06 13:24:28 +0200

    Use A->Z sorting in search if no search term is provided

--- src/app/simple-search/simple-search-form.component.ts
+++ src/app/simple-search/simple-search-form.component.ts
@@ -613,6 +613,8 @@ export class SimpleSearchFormComponent implements OnInit {
         this.page = p;
         this.rows = rows;
         this.sort = sort;
+        if (this.model.q == "")
+          sort = "{LANG}_label asc";
         this.searchIndex = sIdx;
         this.searchTopic = sTop;
         this.searchCallStatus.submit();
@@ -711,6 +713,8 @@ export class SimpleSearchFormComponent implements OnInit {
         this.page = p;
         this.rows = rows;
         this.sort = sort;
+        if (this.model.q == "")
+          sort = "{LANG}_brandName asc";
         this.searchTopic = sTop;
         this.searchCallStatus.submit();
         this.simpleSearchService.getCompFields()
