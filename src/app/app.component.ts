b39324ccc5b9b778486a7d7d0d95300c25e33f4a
Suat Gonul <suat@srdc.com.tr>
2020-09-24 16:34:05 +0300

    Initial design of the homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -483,7 +483,7 @@ export class AppComponent implements OnInit, AfterViewInit {
                 console.log("Loading route " + link);
             if (!this.cookieService.get("user_id")) {
                 if (link != "/" && link != "/user-mgmt/login" && link != "/user-mgmt/registration" && link != "/analytics/info"
-                    && link != "/analytics/members" && link != "/user-mgmt/forgot" && link != "/user-mgmt/logout") {
+                    && link != "/analytics/members" && link != "/user-mgmt/forgot" && link != "/user-mgmt/logout" && link != "/homepage") {
                     this.isLoggedIn = false;
                     this.router.navigate(["/user-mgmt/login"], { queryParams: { redirectURL: url } });
                 }
---
+++ src/app/homepage/homepage.module.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020
+ * AIDIMME - Technological Institute of Metalworking, Furniture, Wood, Packaging and Related sectors; Valencia; Spain
+   In collaboration with
+ * SRFG - Salzburg Research Forschungsgesellschaft mbH; Salzburg; Austria
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+import {NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HomepageComponent} from './homepage.component';
+import {HomepageRoutingModule} from './homepage-routing.module';
+import {CarouselModule} from 'ngx-owl-carousel-o';
+import {CommonModule} from '@angular/common';
+import {AppCommonModule} from '../common/common.module';
+
+@NgModule({
+    imports: [
+        NgbModule,
+        AppCommonModule,
+        CommonModule,
+        HomepageRoutingModule,
+        CarouselModule
+    ],
+    declarations: [
+        HomepageComponent
+    ],
+    exports: [],
+    providers: [],
+    entryComponents: []
+})
+
+export class HomepageModule { }
